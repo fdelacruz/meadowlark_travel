40a49a9816329f7ee0ded664d59f4d7ee83e15de
Francisco De La Cruz <helloquico@gmail.com>
2015-06-21 22:41:56 -0400

    Add newsletter sign-up form (not hooked up)

--- meadowlark.js
+++ meadowlark.js
@@ -19,6 +19,7 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next) {
@@ -96,6 +97,18 @@ app.get('/data/nursery-rhyme', function(req, res){
 		noun: 'heck',
 	});
 });
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
@@ -109,6 +122,7 @@ app.use(function(err, req, res, next) {
 	res.status(500);
 	res.render('500');
 });
+
 app.listen(app.get('port'), function() {
 	console.log('Express started on http:/localhost:' +
 		app.get('port') + '; press Ctrl-C to terminate.');
